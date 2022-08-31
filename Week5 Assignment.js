047ae34a711bfef8693c228d251b7fed5a90fe43
Jason Ackley <Sparkyack@gmail.com>
2022-08-31 11:04:55 -0500

    Final Code for Menu

--- Week5 Assignment.js
+++ Week5 Assignment.js
@@ -17,7 +17,7 @@ class Department{
 
     addEmployee(Employee){
         if(Employee instanceof Employee){
-            this.Employee.push(Employee):
+            this.Employee.push(Employee);
         } else {
             throw new Error(`You can only add current Employees, Argument is not an Employee ${Employee}`)
         }
@@ -30,7 +30,7 @@ class Department{
 class Menu{
     constructor(){
         this.departments =[];
-        this.selecteddepartment = null;
+        this.selectedDepartment = null;
     }
     start(){
         let selection = this.showMainMenuOptions();
@@ -66,12 +66,72 @@ class Menu{
         `);
     }
 
+    showDepartmentMenuOptions(departmentInfo){
+        return prompt(`
+        0) Back
+        1) Create New Employee
+        2) Delete Existing Employee
+        ---------------------------
+        ${departmentInfo}
+        `);
+    }
+    
     viewAllDepartments(){
         let departmentString = "";
-        for( i = 0; i < this.departments.length; i++){
+        for(let i = 0; i < this.departments.length; i++){
             departmentString += i + ") " + this.departments[i].name + "\n";
         }
         alert(departmentString);
     }
 
+    createDepartment(){
+        let name = prompt ("Enter Name of New Department");
+        this.departments.push(new Department(name));
+    }
+
+    viewDepartment(){
+        let index = prompt("Enter the Index of the Department you wish to View:");
+        if(index > -1 && index < this.departments.length){
+            this.selectedDepartment = this.departments[index];
+            let decription = "Department Name: " + this.selectedDepartment.name + "\n";
+
+            for(let i = 0; i < this.selectedDepartment.Employees.length; i++){
+                decription += i + ") " + this.selectedDepartment.Employees[i].name
+                 + " - " + this.selectedDepartment.Employees[i].position + "\n";
+            }
+
+            let selection = this.showDepartmentMenuOptions(decription);
+            switch(selection){
+                case "1":
+                    this.createNewEmployee();
+                    break;
+                case "2":
+                    this.deleteExistingEmployee();
+                    break;
+            }
+        }
+    }
+
+    deleteDepartment(){
+        let index = prompt("Enter the Department you wish to Delete:");
+        if(index > -1 && index < this.departments.length){
+            this.departments.splice[index,1];
+        }
+    }
+
+    createNewEmployee(){
+        let name = prompt("Enter name of new Employee:");
+        let position = prompt("Enter Position for New Employee:");
+        this.selectedDepartment.Employees.push(new Employee(name, position));
+    }
+
+    deleteExistingEmployee(){
+        let index = prompt("Enter the Index of the Employee you wish to Delete:");
+        if(index > -1 && index < this.selectedDepartment.Employees.length){
+            this.selectedDepartment.Employees.splice(index, 1);
+        }
+    }
 }
+
+let menu = new Menu();
+menu.start();
